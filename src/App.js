f40c2030547adc21dca4495a86c2004ca9610b94
chinmayee20 <cpani@purecodesoftware.com>
2024-02-14 18:18:17 +0530

    layout_main extracted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css'
-import Login from './components/Login'
 import { CssBaseline, StyledEngineProvider } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
 import { useSelector } from 'react-redux'
 import themes from './themes'
+import MainLayout from './components/MainLayout'
 
 function App() {
     const customization = useSelector((state) => state.customization)
@@ -12,7 +12,7 @@ function App() {
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={themes(customization)}>
         <CssBaseline />
-        <Login />
+        <MainLayout />
       </ThemeProvider>
     </StyledEngineProvider>
   )
