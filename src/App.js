f86744b9f4497808cecd716eaac9460507e55dd1
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-16 11:07:41 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { useSelector } from 'react-redux'
+import themes from './themes'
+import MainLayout from './components/MainLayout'
+
+function App() {
+    const customization = useSelector((state) => state.customization)
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <CssBaseline />
+        <MainLayout />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  )
+}
+
+export default App
---
+++ src/components/MainLayout/Typography/constant.js
@@ -0,0 +1,4 @@
+// theme
+export const gridSpacing = 3;
+export const drawerWidth = 260;
+export const appDrawerWidth = 320;
