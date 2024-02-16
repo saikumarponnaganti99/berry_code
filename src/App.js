6b17ad3cad2b1d662c859ec2516ffbb4d3d7f275
susmitha <surisettysusmitha@gmail.com>
2024-02-16 12:17:53 +0530

    layout_original without errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { useSelector } from 'react-redux'
+import themes from './components/themes'
+import MainLayout from './components/MainLayout'
 
 function App() {
+    const customization = useSelector((state) => state.customization)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <CssBaseline />
+        <MainLayout />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MainLayout/LogoSection/index.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+// material-ui
+import { ButtonBase, Link } from '@mui/material';
+
+// project imports
+import config from '../../../config';
+import Logo from '../../../assets/images/Logo';
+import { MENU_OPEN } from '../../store/actions';
+
+// ==============================|| MAIN LOGO ||============================== //
+
+const LogoSection = () => {
+  const defaultId = useSelector((state) => state.customization.defaultId);
+  const dispatch = useDispatch();
+  return (
+    <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
+      <Logo />
+    </ButtonBase>
+  );
+};
+
+export default LogoSection;
