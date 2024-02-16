b58ce24025c8e8888a541d820ed960c53ee6a1a3
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-16 15:46:17 +0530

    typorupdate

--- src/components/MainLayout/Typography/CardSecondaryAction.js
+++ src/components/MainLayout/Typography/CardSecondaryAction.js
@@ -3,7 +3,7 @@ import { useTheme } from "@mui/material/styles";
 import { ButtonBase, Link, Tooltip } from "@mui/material";
 
 // project imports
-import Avatar from "../../../components/MainLayout/Typography/Avatar";
+import Avatar from "../../../components/ui-component/extended/Avatar";
 
 // ==============================|| CARD SECONDARY ACTION ||============================== //
 
--- src/store/constant.js
+++ src/store/constant.js
@@ -1,4 +1,4 @@
-// theme constant
+// theme
 export const gridSpacing = 3;
 export const drawerWidth = 260;
 export const appDrawerWidth = 320;
