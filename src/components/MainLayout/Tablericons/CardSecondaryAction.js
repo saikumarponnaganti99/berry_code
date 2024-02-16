0cb35fbf6c4589f090cb080862c45bca0260676a
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-16 15:43:01 +0530

    tablerupdate

--- src/components/MainLayout/Tablericons/CardSecondaryAction.js
+++ src/components/MainLayout/Tablericons/CardSecondaryAction.js
@@ -3,7 +3,7 @@ import { useTheme } from "@mui/material/styles";
 import { ButtonBase, Link, Tooltip } from "@mui/material";
 
 // project imports
-import Avatar from "../../../components/MainLayout/Tablericons/Avatar";
+import Avatar from "../../ui-component/extended/Avatar";
 
 // ==============================|| CARD SECONDARY ACTION ||============================== //
 
